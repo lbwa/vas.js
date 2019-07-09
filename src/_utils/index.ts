4d70009da050ec146a0a3aa06b89ccb41263b3e7
lbwa <mr_lbw@outlook.com>
2019-07-09 10:23:37 +0800

    feat: remove redundant type-judgement function

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -1,6 +1,3 @@
 export function assert(condition: any, msg: string) {
   if (!condition) throw new Error(`[vasjs]:${msg}`)
 }
-
-export const isType = (type: string) => (target: any) =>
-  Object.prototype.toString.call(target) === `[object ${type}]`
